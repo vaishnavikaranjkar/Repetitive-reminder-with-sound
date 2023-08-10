8e44c9be8bbde8e3f0e37c8f94698d02f45271f6
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-08-10 15:44:43 +0530

    Added clock

--- App_prod.py
+++ App_prod.py
@@ -1,7 +1,13 @@
 from tkinter import *
 import winsound
+from time import strftime
 import time
 import threading
+
+def clock():
+    string = time.strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, clock)
 
 def ThreadStart():
     t1=threading.Thread(target=alarm)
@@ -11,17 +17,22 @@ def alarm():
     while True:
         min=15*60
         time.sleep(min)
-        print("Time to Drink Water")
         winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
+
 window = Tk()
-window.geometry("400x250")
+window.title("Timer")
+window.geometry("400x300")
 
 Label(window, text="Alarm Clock", font=("Helvetica 20 bold"),fg="red").pack(pady=10)
 Label(window, text="Drink water every 15 minutes", font=("Helvetica 15 bold")).pack()
+
+lbl=Label(window, font=("Helvetica 15 bold"))
+lbl.pack(pady=10)
 
 frame=Frame(window)
 frame.pack()
 
 Button(window,text="Start Alarm",font=("Helvetica 15"),command=ThreadStart).pack(pady=20)
 
+clock()
 window.mainloop()
